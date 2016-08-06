549f5f3b53643349ad716efc0d3d194aa2898a8a
Dmitry Starostin <incrop@gmail.com>
2016-08-06 13:30:32 +0300

    Update path in build.rs

--- xdelta-sys/build.rs
+++ xdelta-sys/build.rs
@@ -6,7 +6,17 @@ use std::os::raw;
 
 fn main() {
     gcc::Config::new()
-        .file("xdelta3.c")
+        .file("xdelta3/xdelta3.c")
+        .flag("-Wall")
+        .flag("-Wshadow")
+        .flag("-fno-builtin")
+        .flag("-Wextra")
+        .flag("-Wsign-compare")
+        .flag("-Wformat=2")
+        .flag("-Wno-format-nonliteral")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wno-unused-function")
+        .flag("-pedantic")
         .flag("-std=c99")
         .flag(&format!("-DSIZEOF_SIZE_T={}", size_of::<usize>()))
         .flag(&format!("-DSIZEOF_UNSIGNED_INT={}", size_of::<raw::c_uint>()))
